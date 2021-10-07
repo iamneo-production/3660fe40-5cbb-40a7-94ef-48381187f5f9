2d3e34b1cf07f2d02c54d52f4c7587288a3fbba9
Ahamed30 <dhanishahmed30@gmail.com>
2021-10-07 01:26:48 +0000

    initial commit

---
+++ springapp/src/main/java/com/examly/springapp/model/UserModel.java
@@ -0,0 +1,96 @@
+package com.examly.springapp.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+@Entity
+public class UserModel implements Serializable{
+    @Id
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String username;
+    private String mobileNumber;
+    private Boolean active;
+    private String role;
+    
+    
+    public UserModel() {
+    }
+
+
+    public UserModel(String email, String password, String username, String mobileNumber, Boolean active, String role) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.mobileNumber = mobileNumber;
+        this.active = active;
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " email='" + getEmail() + "'" +
+            ", password='" + getPassword() + "'" +
+            ", username='" + getUsername() + "'" +
+            ", mobileNumber='" + getMobileNumber() + "'" +
+            ", active='" + getActive() + "'" +
+            ", role='" + getRole() + "'" +
+            "}";
+    }
+
+}
