3bf2dd74ee9a353205e71b17c4cceca80ae11486
manyu462 <MANCHALAMANYU@GMAIL.COM>
2021-10-16 06:50:19 +0000

    authentication pages css fix

--- reactapp/src/Login.js
+++ reactapp/src/Login.js
@@ -6,16 +6,16 @@ function Login()
 {
     return(
         <div className="Login">
-        <div className="heading">
+        <div className="loginheading">
           Mr.Viewer
         </div>
           <form className="form-outer">
-            <h2>Login</h2>
+            <h2 className="logintext" id="logintext">Login</h2>
             <div className="form-input">
               <input type="text" name="email" id="email" placeholder="Enter email"/>
             </div>
             <div className="form-input">
-              <input type="text" name="password" id="password" placeholder="Enter password"/>
+              <input type="password" name="password" id="password" placeholder="Enter password"/>
             </div>
             <p>New User?</p><NavLink className="signup" to="/SignUp">Sign Up</NavLink>
             <button className="btnlgn">Login</button>
