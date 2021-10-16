887ba1fa3346d83dcb1de14d5ccba28d316fcf87
ashajy <kuradaashajyothi123@gmail.com>
2021-10-16 11:05:19 +0000

    git cleaning

---
+++ reactapp/src/movie.js
@@ -0,0 +1,11 @@
+import React from "react";
+function card()
+{
+    return (<div>
+        <h1> this is rightside </h1>
+        <br/>
+         <p>
+             hey hey hey what are you doing. oyyy!.
+         </p>
+    </div>)
+}
